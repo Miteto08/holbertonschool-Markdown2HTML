6500feb3d4e43b04d1d322b1f3d1aa9253fa9087
Miteto08 <julien.lechevalier@shortways.com>
2023-12-11 13:18:05 +0100

    task 0 cor 1

--- markdown2html.py
+++ markdown2html.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import sys
-import os
+from os import path
 
 if len(sys.argv) != 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     exit(1)
 
-markdown_file = "README.md"
-html_file = "README.html"
-
-if not os.path.exists(markdown_file):
-    print(f"Missing {markdown_file}", file=sys.stderr)
+if not path.exists(sys.argv[1]):
+    print(f"Missing {sys.argv[1]}", file=sys.stderr)
     exit(1)
 
 exit(0)
