cc0105740b518ba31d2de34ff760e3f94715dd50
Miteto08 <julien.lechevalier@shortways.com>
2023-12-11 13:23:48 +0100

    task 0 cor 2

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+''' Markdown to HTML '''
 import sys
 from os import path
 
-if len(sys.argv) != 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    exit(1)
 
-if not path.exists(sys.argv[1]):
-    print(f"Missing {sys.argv[1]}", file=sys.stderr)
-    exit(1)
-
-exit(0)
+if __name__ == "__main__":
+    '''the number of arguments is less than 2'''
+    if len(sys.argv) != 3:
+        print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
+        exit(1)
+    '''Markdown file doesn’t exist'''
+    if not path.exists(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
