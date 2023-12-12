810440c0b52f23ac076041d66e378923e2c3e380
Miteto08 <julien.lechevalier@shortways.com>
2023-12-12 11:33:43 +0100

    task 1 cor 7

--- markdown2html.py
+++ markdown2html.py
@@ -1,29 +1,56 @@
 #!/usr/bin/python3
-''' Markdown to HTML '''
-import sys
-from os import path
+"""
+Test case
+"""
+import os
+import random
+import string
+import time
+from bs4 import BeautifulSoup
+
+def random_string(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
 
-if __name__ == "__main__":
-    '''the number of arguments is less than 2'''
-    if len(sys.argv) != 3:
-        print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
+fn_md = "{}.md".format(random_string())
+fn_html = "{}.html".format(random_string())
+
+# remove initial MD file
+if os.path.exists(fn_md):
+    os.remove(fn_md)
+
+# remove initial HTML file
+if os.path.exists(fn_html):
+    os.remove(fn_html)
+
+# create MD file
+c = """
+# title
+"""
+with open(fn_md, 'w') as f:
+    f.write(c)
+
+# run
+os.system("./markdown2html.py {} {}".format(fn_md, fn_html))
+time.sleep(1)
+
+# parse HTML file
+with open(fn_html, "r") as f:
+    root = BeautifulSoup(f.read(), 'html.parser')
+    all_tag_names = [tag.name for tag in root.find_all()]
+    if len(all_tag_names) == 0:
+        print("No tag found")
         exit(1)
-    '''Markdown file doesn’t exist'''
-    if not path.exists(sys.argv[1]):
-        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-        exit(1)
-    '''Headings Markdown'''
-    with open(sys.argv[1], 'r') as read_file:
-        line_list = []
-        for lines in read_file.readlines():
-            number_of_hashes = 0
-            for line in lines:
-                for car in range(len(line)):
-                    if line[car] == '#':
-                        number_of_hashes += 1
-            lines = lines.rstrip('\r\n')
-            line_list.append("<h{}>{}</h{}>".format(number_of_hashes, lines.replace('#',''), number_of_hashes))
-        with open(sys.argv[2], 'a') as write_file:
-            for line in line_list:
-                write_file.write('{}\n'.format(line))
+    if len(all_tag_names) != 1:
+        print("Too many tags: {}".format(all_tag_names))
+    if all_tag_names[0] != 'h1':
+        print("H1 not found: {}".format(all_tag_names[0]))
+    print("OK", end="")
+
+# delete files
+if os.path.exists(fn_md):
+    os.remove(fn_md)
+if os.path.exists(fn_html):
+    os.remove(fn_html)
